2376226c909877a8555eb16773353a8b9cfddadd
Rizwan zafar <rizwanzafar441@gmail.com>
2026-04-30 22:30:47 +0500

    thi is second ci

--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,7 @@
+const test = require('node:test');
+const assert = require('node:assert');
 const app = require('./app');
 
 test('App should be defined', () => {
-  expect(app).toBeDefined();
+  assert.ok(app);
 });
