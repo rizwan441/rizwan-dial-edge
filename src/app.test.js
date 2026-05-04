99b05762066e27f3a3c1dfb448171fe8287fc5b9
Rizwan zafar <rizwanzafar441@gmail.com>
2026-05-04 22:18:07 +0500

    ingrresscontroller setup and setup local to domain

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,7 @@
 const test = require('node:test');
 const assert = require('node:assert');
 const app = require('./app');
-
+console.log('🔍 Running tests...');
 test('App should be defined', () => {
   assert.ok(app);
 });
